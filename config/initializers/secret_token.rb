5cbd4465dd187495772a8cc8ac667b89b4b3aa91
Rob Di Marco <rob@innovationontherun.com>
2012-04-11 07:29:04 -0400

    Initial rails create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleRemoteForm::Application.config.secret_token = '2a946dcc5317ef1eb574746e347c9107179af5fa0359e9409e4a901e2d0d4474a137bdab549f5ea38d33b5f6f9bb5637862204455107d32a51f828df6de2c3bc'
